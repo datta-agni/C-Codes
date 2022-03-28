61077f646e1a9653bfb5679c3312bbc50ca86147
Agni Datta <dattadunga@gmail.com>
2022-03-28 20:04:36 +0530

    Added files.

--- Codes/Optimization/job_scheduling.c
+++ Codes/Optimization/job_scheduling.c
@@ -3,12 +3,12 @@
 int main() {
     int bt[20], p[20], wt[20], tat[20], i, j, n, total = 0, pos, temp;
     float avg_wt, avg_tat;
-    printf("Enter number of process:");
+    printf("Enter number of process : ");
     scanf("%d", &n);
 
-    printf("nEnter Burst Time:n");
+    printf("\nEnter Burst Time : \n");
     for (i = 0; i < n; i++) {
-        printf("p%d:", i + 1);
+        printf("p%d : ", i + 1);
         scanf("%d", &bt[i]);
         p[i] = i + 1;
     }
@@ -22,10 +22,9 @@ int main() {
         temp    = bt[i];
         bt[i]   = bt[pos];
         bt[pos] = temp;
-
-        temp   = p[i];
-        p[i]   = p[pos];
-        p[pos] = temp;
+        temp    = p[i];
+        p[i]    = p[pos];
+        p[pos]  = temp;
     }
 
     wt[0] = 0;
@@ -39,17 +38,17 @@ int main() {
     avg_wt = (float)total / n;
     total  = 0;
 
-    printf("nProcesst    Burst Time    tWaiting TimetTurnaround Time");
+    printf("\nProcess    \tBurst Time    \tWaiting Time   \tTurnaround Time");
     for (i = 0; i < n; i++) {
         tat[i] = bt[i] + wt[i];
         total += tat[i];
-        printf("np%dtt  %dtt    %dttt%d", p[i], bt[i], wt[i], tat[i]);
+        printf("\np%d \t\t %d  \t\t %d  \t\t %d", p[i], bt[i], wt[i], tat[i]);
     }
 
     avg_tat = (float)total / n;
 
-    printf("nnAverage Waiting Time=%f", avg_wt);
-    printf("nAverage Turnaround Time=%fn", avg_tat);
+    printf("\n\nAverage Waiting Time = %f", avg_wt);
+    printf("\n\nAverage Turnaround Time = %f\n", avg_tat);
 
     return 0;
 }
